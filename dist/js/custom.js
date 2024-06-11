e1b09c7496dc59fe60951d2d0f572f28ffc4c696
yooonmi12 <122426957+yooonmi12@users.noreply.github.com>
2024-06-11 17:54:35 +0900

    메인 진행중

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -45,7 +45,7 @@ window.addEventListener("load", ()=>{
 
 });
 
-
+// 사이드메뉴 스크립트
 const sideMenu = ()=>{
     // Side Menu
     $(".side-menu").on("click", function () {
@@ -79,6 +79,7 @@ const sideMenu = ()=>{
     });
 }
 
+// 헤더 스크립트
 const headerScript = ()=>{
 
     $(".quick_menu ul li.type02 > div > a").on('click',function(){
@@ -99,4 +100,68 @@ const headerScript = ()=>{
         $('body').removeClass('overflow-hidden')
     });
 
+}
+
+
+$(window).on('scroll',function(){
+    // 스크롤시 회원가입캐시백 팝업 닫기
+    let footT = $("footer").offset().top - $(window).height();
+    let scrollTop = $(window).scrollTop()
+    if(footT < scrollTop){
+        $('.cashback_popup').removeClass('open')
+    }else{
+        $('.cashback_popup').addClass('open')
+    }
+
+})
+
+
+// 스와이퍼 공통
+$('.mySwiper').each(function(index) {
+    var mySwiper = $(this),
+        swiperContainer = $(this).find('.swiper-container'),
+        itemPer = $(this).attr('data-per') ? $(this).attr('data-per') : 1,
+        itemGap = $(this).attr('data-gap') ? $(this).attr('data-gap') : 0,
+        slideLoop = $(this).attr('data-loop') == 'false' ? false : true,
+        slideCenter = $(this).attr('data-center') == 'true' ? true : false,
+        slidePlayTime = $(this).attr('data-timer') ? $(this).attr('data-timer') * 1000 : 0;
+        effect = $(this).attr('data-effect') ? $(this).attr('data-effect') : 'slide';
+    $(this).addClass('num'+index);		
+    var swiper =  new Swiper( '.mySwiper.num' + index + ' .swiper-container', {
+        spaceBetween: parseInt(itemGap),
+        slidesPerView: itemPer == 'auto' ? "auto" : itemPer,
+        effect: effect,
+        pagination: {
+            el: '.mySwiper.num' + index + ' .pagination',
+            clickable: true,
+            type:  $('.mySwiper.num' + index + ' .pagination').hasClass('fraction') ? "fraction" : "bullets",
+        },
+        navigation: {
+            nextEl: '.mySwiper.num' + index + ' .next',
+            prevEl: '.mySwiper.num' + index + ' .prev'
+        },
+        speed : 1000,
+        centeredSlides: slideCenter,
+        autoplay: slidePlayTime ? {delay: parseInt(slidePlayTime),disableOnInteraction:true} : false,
+        loop: slideLoop			
+    });		
+    if($(this).attr('data-slideto') == '1') {
+        $(slideWrapper.find('.swiper-slide')).click(function() {
+            var i = $(this).index();
+            swiper.slideTo(i,700,false);
+        });
+    }
+});
+
+
+// 햄버거메뉴
+const hamToggle = ()=>{
+    $('.side_menu').toggleClass('open');
+    $('.mo_menu_bg').toggleClass('open');
+    $('body').toggleClass('overflow-hidden');
+}
+
+// 닫기 - 공통
+const parentClose = (item)=>{
+    $(item).addClass('!hidden')
 }
